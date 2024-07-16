e8ae546fcf7bfe53b45f70c4f9a9ba0b1dbb5879
Srinath0407 <161798714+Srinath0407@users.noreply.github.com>
2024-07-16 09:36:01 +0530

    Add files via upload

--- add.java
+++ add.java
@@ -1,9 +1,12 @@
+
 class add
 {
-    public static void main(String args[])
-        {
-         int a=10;
-         int b=20;
-         System.out.println("Sum :"+(a+b));
-        }
+w
+    {
+        int a,b,c;
+        a=10;
+        b=33;
+        c=a+b;
+        System.out.println("sum of a and b = "+c);
+    }
 }
--- area.java
+++ area.java
@@ -1,9 +1,11 @@
-class Area {
-    public static void main(String[] args) {
-        double l, b, area;
-        l = 5;
-        b = 12;
-        area = (l * b) / 2;
-        System.out.println("Area of triangle: " + area);
+class area
+{
+    public static void main(String args[])
+    {
+        double r,d,c;
+        d=3.14;
+        r=7;
+        c=d*r*r;
+        System.out.println("area of crcule = "+c);
     }
 }
