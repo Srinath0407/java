7e684549cb9149803931001fc1a73296e12af2e9
Srinath0407 <161798714+Srinath0407@users.noreply.github.com>
2024-08-06 09:21:57 +0530

    Add files via upload

--- ASSIGNMENT/a1.java
+++ ASSIGNMENT/a1.java
@@ -1,27 +1,75 @@
-import java.util.*;
-class Rect{
-    double a;
-    Rect(double x, double y, double z){
-        a = (x*y*z);
+abstract class Shape {
+    abstract double area();
+    abstract double volume();
+}
+
+class Sphere extends Shape {
+    double radius;
+
+    Sphere(double r) {
+        radius = r;
     }
 
-    void display(){
-        System.out.println("Area of the rectangle is: " + a);
+    double area() {
+        return 4 * Math.PI * radius * radius;
+    }
+
+    double volume() {
+        return (4.0 / 3) * Math.PI * Math.pow(radius, 3);
     }
 }
-public class a1 {
-    public static void main(String[] args) {
-        double length, breadth, height;
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the length: ");
-        length = sc.nextDouble();
-        System.out.print("Enter the breadth: ");
-        breadth = sc.nextDouble();
-        System.out.print("Enter the height: ");
-        height = sc.nextDouble();
-        Rect obj = new Rect(length, breadth, height);
-        obj.display();
-        sc.close();
+
+class Cone extends Shape {
+    double radius;
+    double height;
+
+    Cone(double r, double h) {
+        radius = r;
+        height = h;
     }
 
+
+    double area() {
+        double slantHeight = Math.sqrt(radius * radius + height * height);
+        return Math.PI * radius * (radius + slantHeight);
+    }
+
+    double volume() {
+        return Math.PI * radius * radius * height / 3;
+    }
+}
+
+class Cylinder extends Shape {
+    double radius;
+    double height;
+
+    Cylinder(double r, double h) {
+        radius = r;
+        height = h;
+    }
+
+    double area() {
+        return 2 * Math.PI * radius * (radius + height);
+    }
+
+    double volume() {
+        return Math.PI * radius * radius * height;
+    }
+}
+
+public class a1 {
+    public static void main(String[] args) {
+        Shape sphere = new Sphere(5);
+        Shape cone = new Cone(3, 7);
+        Shape cylinder = new Cylinder(4, 10);
+
+        System.out.println("Sphere Area: " + sphere.area());
+        System.out.println("Sphere Volume: " + sphere.volume());
+
+        System.out.println("Cone Area: " + cone.area());
+        System.out.println("Cone Volume: " + cone.volume());
+
+        System.out.println("Cylinder Area: " + cylinder.area());
+        System.out.println("Cylinder Volume: " + cylinder.volume());
+    }
 }
--- ASSIGNMENT/a2.java
+++ ASSIGNMENT/a2.java
@@ -1,46 +1,34 @@
-import java.util.*;
-class Stud{
-    String a, b;
-    int c;
-
-    void getReg(String x){
-        a = x;
+class Movable {
+    public void move() {
+        System.out.println("Moving...");
     }
-    void getName(String y){
-        b = y;
-    }
-    void getMarks(int z){
-        c = z;
-    }
-
-    void display(){
-        System.out.println("Registration number: " + a);
-        System.out.println("Name: " + b);
-        System.out.println("Marks: " + c);
-    }
-
 }
+
+interface Drivable {
+    void drive();
+}
+
+interface Flyable {
+    void fly();
+}
+
+class Vehicle extends Movable implements Drivable, Flyable {
+    @Override
+    public void drive() {
+        System.out.println("Driving...");
+    }
+
+    @Override
+    public void fly() {
+        System.out.println("Flying...");
+    }
+}
+
 public class a2 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String RegNo, name;
-        int marks;
-        Stud obj = new Stud();
-
-        System.out.print("Enter the registration number: ");
-        RegNo = sc.nextLine();
-        obj.getReg(RegNo);
-        
-        System.out.print("Enter the name: ");
-        name = sc.nextLine();
-        obj.getName(name);
-        
-        System.out.print("Enter the marks: ");
-        marks = sc.nextInt();
-        obj.getMarks(marks);
-        
-        obj.display();
-        
-        sc.close();
+        Vehicle vehicle = new Vehicle();
+        vehicle.move();
+        vehicle.drive();
+        vehicle.fly();
     }
 }
